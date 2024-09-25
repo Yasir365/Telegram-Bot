78912ba2d85312cb7a952a5afd058bf02d57b754
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-09-25 10:53:53 +0500

    hero update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,83 +3,59 @@ import './hero.scss';
 
 export default function Hero() {
     return (
-        <>
-            <div className="ln-content">
-                <div className="ln-content-hamster">
-                    <div className="ln-content-hamster-image is-1">
-                        <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
-                    </div>
-                    <div className="ln-content-hamster-image is-2">
-                        <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
-                    </div>
-                    <div className="ln-content-hamster-image is-3">
-                        <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
-                    </div>
-                    <div className="ln-content-hamster-image is-4">
-                        <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
-                    </div>
+        <div className='hero'>
+            <div className="overlay">
+                <div className="overlay-image is-1">
+                    <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
                 </div>
-                <h1>Unleash your inner CEO</h1>
-                <div className="ln-content-exchanges">
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-binance">
-                            <img src="/svgs/exchange1.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-bybit">
-                            <img src="/svgs/exchange2.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-crypto_com">
-                            <img src="/svgs/exchange1.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-okx">
-                            <img src="/svgs/exchange2.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-bingx">
-                            <img src="/svgs/exchange1.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-htx">
-                            <img src="/svgs/exchange2.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-kucoin">
-                            <img src="/svgs/exchange1.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-gate_io">
-                            <img src="/svgs/exchange2.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-mexc">
-                            <img src="/svgs/exchange1.svg" alt="" />
-                        </div>
-                    </div>
-                    <div className="ln-content-exchange">
-                        <div className="exchange-image is-bitget">
-                            <img src="/svgs/exchange2.svg" alt="" />
-                        </div>
-                    </div>
+                <div className="overlay-image is-2">
+                    <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
                 </div>
-                <p> Make your way from the shaved hamster to the grandmaster CEO of the tier-1 crypto exchange </p>
-                <p className="is-hidden-mobile"> Buy upgrades, complete quests, invite friends and become the best </p>
-                <div className="ln-content-button">
-                    <a className="button" href="#" target="_blank" >
-                        <span>Play now</span>
-                    </a>
+                <div className="overlay-image is-3">
+                    <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
+                </div>
+                <div className="overlay-image is-4">
+                    <img className="img-responsive" src='/images/kambat.png' alt="Hamster Kombat" />
                 </div>
             </div>
-        </>
+            <h1>Unleash your inner CEO</h1>
+            <div className="exchanges">
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange1.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange2.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange1.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange2.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange1.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange2.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange1.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange2.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange1.svg" alt="" />
+                </div>
+                <div className="image-wrapper">
+                    <img src="/svgs/exchange2.svg" alt="" />
+                </div>
+            </div>
+            <p> Make your way from the shaved hamster to the grandmaster CEO of the tier-1 crypto exchange </p>
+            <p className="hidden-mobile"> Buy upgrades, complete quests, invite friends and become the best </p>
+            <div className="button-container">
+                <a className="button" href="#"> <span>Play now</span> </a>
+            </div>
+        </div>
     )
 }
